7262b5325f03775c46d00cd427648838da104005
AndiMandi <Andre200344@bk.ru>
2025-08-01 01:27:55 +0700

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header d-flex">
+    <div class="header__items align-item-center">
+      <div class="header__left-side">
+        <div class="header__logo">
+          <h1 class="header__logo-text blue">Каталог</h1>
+        </div>
+      </div>
+      <div class="header__right-side">
+        <nav class="header__nav d-flex">
+          <ul>
+            <li><a href="#">СРАВНЕНИЕ</a></li>
+            <li><a href="#">Личный кабинет <img src="../assets/img/LK.svg"></a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
